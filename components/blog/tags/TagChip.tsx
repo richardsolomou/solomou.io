58938d59f74f12e081432936eae68042227069f9
Richard Solomou <richard@solomou.io>
2023-01-02 02:48:56 +0200

    rename Tag to TagChip

--- components/blog/tags/TagChip.tsx
+++ components/blog/tags/TagChip.tsx
@@ -3,10 +3,10 @@ import NextLink from 'next/link';
 
 import { TagInterface } from '../../../lib/tags';
 
-export const Tag: React.FC<TagInterface> = (tag) => {
+export const TagChip: React.FC<TagInterface> = (tag) => {
   return (
     <NextLink href="/blog/tags/[slug]" as={`/blog/tags/${tag.slug}`} passHref legacyBehavior>
-      <Chip key={tag.slug} size="small" label={tag.title} sx={{ mr: 0.5, mt: 0.5 }} clickable />
+      <Chip key={tag.slug} size="small" label={tag.title} sx={{ mr: 0.5, mt: 0.5 }} clickable color="primary" />
     </NextLink>
   );
 };
--- components/blog/tags/TagChips.tsx
+++ components/blog/tags/TagChips.tsx
@@ -2,13 +2,13 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 import { TagInterface } from '../../../lib/tags';
-import { Tag } from './Tag';
+import { TagChip } from './TagChip';
 
-export const Tags: React.FC<{ tags: TagInterface[] }> = ({ tags }) => {
+export const TagChips: React.FC<{ tags: TagInterface[] }> = ({ tags }) => {
   return (
     <Box sx={{ display: 'flex', mt: 1 }}>
       {tags.map((tag) => (
-        <Tag key={tag.slug} {...tag} />
+        <TagChip key={tag.slug} {...tag} />
       ))}
     </Box>
   );
