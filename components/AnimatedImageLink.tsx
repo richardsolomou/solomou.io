6bf08ad00682216ce20a9dda5ce7db517441a7bb
Richard Solomou <richard@solomou.io>
2023-11-13 18:04:42 +0200

    fix deployment

--- components/AnimatedImageLink.tsx
+++ components/AnimatedImageLink.tsx
@@ -1,8 +1,8 @@
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material';
 
 import { ExternalLink } from './common/ExternalLink';
 
-export const AnimatedImageLink = styled(ExternalLink)({
+export const AnimatedImageLink: any = styled(ExternalLink)({
   display: 'flex',
   transition: '0.1s transform',
   '&:hover, &:focus, &:active': { transform: 'scale(1.1)' },
